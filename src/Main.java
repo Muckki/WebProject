a97c73fbc9737b580f25885ffee3756c33e8c84f
Muckki <ckdduq0508@gmail.com>
2023-10-11 00:35:28 +0900

    2023-10-10 1일차 게시판까지

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==========프로그램 시작==========");
+		System.out.println("==프로그램 시작==");
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		int articleNum = 1;
 		
-		int command = sc.nextInt();
-		
-		if ( command == 1 ) {
-			System.out.println("자유게시판입니다.");
+		List<Article> articles = new ArrayList<>();
+
+		while (true) {
+			System.out.println("명령어 ) ");
+			String command = scanner.nextLine();
+			if(command.equals("article list")) {
+				if(articles.size() != 0) {
+					System.out.println("번호   /  제목");
+					for(int i = articles.size(); i>0; i--) {
+						Article article = articles.get(i-1);
+						System.out.println(article.articleNum + "   /   " + article.title);						
+					}
+				}else {
+					System.out.println("게시글이 없습니다.");					
+				}
+			}else if (command.equals("article write")) {
+				System.out.println("제목 : ");
+				String title = scanner.nextLine();
+				System.out.println("내용 : ");
+				String body = scanner.nextLine();
+				System.out.println(articleNum + "번글이 생성되었습니다.");
+				
+				Article article = new Article(articleNum, title,body);
+				articles.add(article);
+				
+				articleNum += 1;
+			}else if( command.equals("exit")) {
+				break;
+			}else if( command.length() < 1) {
+				System.out.println("명령어를 입력해주세요");
+			}
+			else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+			}
 		}
-		int nextCommand = sc.nextInt();
-		if( nextCommand == 2) {
-			System.out.println("퇴장하십니다.");
-		}
 		
-		System.out.println("==========프로그램 끝==========");
-		sc.close();
+		
+		scanner.close();
+		System.out.println("==프로그램 끝==");
+	}
+}
+
+class Article {
+	int articleNum;
+	String title;
+	String body;
+	
+	Article(int articleNum, String title, String body){
+		this.articleNum = articleNum;
+		this.title = title;
+		this.body = body;
 	}
 }
